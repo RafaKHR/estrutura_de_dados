f123d1a9f18840d9dd15b937df4fe4714033e1a4
Rafael <111534933+RafaKHR@users.noreply.github.com>
2023-08-19 11:16:47 -0300

    Add files via upload

--- Aula 02/main.c
+++ Aula 02/main.c
@@ -2,30 +2,36 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <conio.h>
+#include <string.h>
 
-
-/*  */
+/* O Yugi falou comigo */
 
 typedef struct{
-	float Peso;
-	int Idade;
-	float Altura;
-}Pessoa;
+	char Titulo[50];
+	int Classificacao;
+	char Diretor[50];
+	char Genero[50];
+	char Duracao[50];
+}Filme;
 
-void imprimePessoa(Pessoa P){
-	printf("Idade: %d Peso: %.2f Altura: %.2f", P.Idade, P.Peso, P.Altura);
+void setFilme(int classificacao, const char *titulo, const char *diretor, const char *duracao, const char *genero){
+	Filme F;
+	F.Classificacao = classificacao;
+	strcpy(F.Titulo, titulo);
+	strcpy(F.Diretor, diretor);
+	strcpy(F.Genero, genero);
+	strcpy(F.Duracao, duracao);
 };
-void setPessoa(Pessoa *P, float peso, int idade, float altura){
-	(*P).Peso = peso;
-	P->Idade = idade;
-	P->Altura = altura;
-}
 
-int main(){
+void imprimeFilme(Filme F){
+	printf("Título do filme: %s \nClassificação do filme: %d \nDiretor do filme: %s \nGênero do filme: %s \nDuração do filme: %s", F.Classificacao, F.Titulo, F.Diretor, F.Duracao, F.Genero);
+};
+
+int main(void){
 	setlocale(LC_ALL, "Portuguese");
 	system("color 5e");
-	Pessoa Daniel;
-	setPessoa(&Daniel, 67.7,20,1.79);
-	imprimePessoa(Daniel); 
+	Filme Corra;
+	Corra = setFilme(14, "Corra", "1 hora e 44 minutos", "Jordan Peele", "Terror");
+	imprimeFilme(Corra);
 	return 0;
 }
